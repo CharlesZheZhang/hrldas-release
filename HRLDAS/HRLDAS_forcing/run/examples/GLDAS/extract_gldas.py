0a0835a3c1578c5b8f398ff6f11858fae8d1348d
RJVogel <julian.r.vogel@gmail.com>
2020-08-10 10:37:47 +0200

    Bugfix for MOD(X,0) and mod. of Py scripts
    create_forcing_netcdf.F: The problem was that with my compiler (gfortran 9.3) the code checks if full_ic_frq>0 and if it is zero, it also checks (mod(ihour, full_ic_frq)==0). Then for the mod function both arguments are zero, which leads to an arithmetic error, since MOD(X,0) is not allowed. I guess that other compilers either do not check the second argument of an AND, when the first is false, or they define that MOD(x,0)=0. My workaround is long and it needs another variable. Maybe you can come up with a better solution.
    
    module_NoahMP_hrldas_driver.F: The problem here was again that MOD(x,0) was called and an error “invalid arithmetic operation” was thrown. I took the easy way here and split the “IF AND” condition up into two IF conditions, which worked for me since I don’t use RUNOFF_OPTION 5. ;) Of course this is not a permanent solution, but I am not sure what should happen for STEPWTD==0, if the code segment should run or not, so I left it at that.
    
    Python scripts: The indentation was not correct in my editor (Spyder 4), probably due to tabstopps. Also the print statements did not work, I had to put the arguments in brackets, maybe due to a different Python version? I am using Python 3.8.

--- HRLDAS/HRLDAS_forcing/run/examples/GLDAS/extract_gldas.py
+++ HRLDAS/HRLDAS_forcing/run/examples/GLDAS/extract_gldas.py
@@ -1,67 +1,83 @@
 #!/usr/bin/python
-### Zhe Zhang
-### 2020-04-21
-### python script to extract gldas variables
-### origional perl code from Mike's directory
-### migrate from perl to python 
+# Zhe Zhang
+# 2020-04-21
+# python script to extract gldas variables
+# origional perl code from Mike's directory
+# migrate from perl to python
 
 import glob
-import numpy as np
-import sys,os
+import os
 
-### create date arrays
-nums = ["00","01","02","03","04","05","06","07","08","09",
-        "10","11","12","13","14","15","16","17","18","19",
-        "20","21","22","23","24","25","26","27","28","29",
-        "30","31"]
-yrs  = ["00"]
-day_start = 270 
-day_end   = 280
-hrs = ["00","03","06","09","12","15","18","21"]
-cc  = ["20"]   # Manually set the century
+# Configure start and end day
+day_start = 1
+day_end = 3
 
-noleap_days = [0,31,59,90,120,151,181,212,243,273,304,334,365]
-leap_days   = [0,31,60,91,121,152,182,213,244,274,305,335,366]
-vars_name = ["Rainf_tavg","Snowf_tavg","Wind_f_inst","Tair_f_inst",
-             "Qair_f_inst","Psurf_f_inst","SWdown_f_tavg","LWdown_f_tavg"]
-vars_short= ["Rainf","Snowf","Wind","Tair",
-             "Qair","Psurf","SWdown","LWdown"]
-data_dir = "/glade/work/zhezhang/GLDAS/raw"
-results_dir = "/glade/work/zhezhang/GLDAS/extracted"
+# Set directories and file name
+data_dir = "/opt/ARW/DATA/GLDAS/raw/netcdf/"
+results_dir = "/opt/ARW/DATA/GLDAS/extracted/"
+filestem = "GLDAS_NOAH025_3H.A"
+
+# set years
+cc = ["20"]
+yrs = ["10"]
+
+# create date arrays
+nums = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
+        "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+        "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+        "30", "31"]
+hrs = ["00", "03", "06", "09", "12", "15", "18", "21"]
+noleap_days = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
+leap_days = [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366]
+
+# Set var names
+vars_name = ["Rainf_tavg", "Snowf_tavg", "Wind_f_inst", "Tair_f_inst",
+             "Qair_f_inst", "Psurf_f_inst", "SWdown_f_tavg", "LWdown_f_tavg"]
+vars_short = ["Rainf", "Snowf", "Wind", "Tair", "Qair", "Psurf", "SWdown",
+              "LWdown"]
 
 for var in range(len(vars_name)):
+    print("working on variable: ", vars_short[var])
     for yy in range(len(yrs)):
-        for julday in range(day_start,day_end,1):
+        for julday in range(day_start, day_end+1, 1):
+            # check for leap years and select days
             modays = noleap_days
-            if (yrs[yy] == "92" or "96"):
-		modays = leap_days
-	    if (yrs[yy] == "00" or "04" or "08" or "12" or "16"):
-		modays = leap_days
-            for mo in range(0,12,1):
-                if (julday>modays[mo] and julday<=modays[mo+1]):
-		    day = (julday - modays[mo])
-                    if (mo<9):
-		        mon = "0"+str(mo+1)
+            if (yy == "92" or yy == "96" or yy == "00" or yy == "04" or
+                yy == "08" or yy == "12" or yy == "16" or yy == "20" or
+                yy == "24" or yy == "28" or yy == "32" or yy == "36" or
+                    yy == "40" or yy == "44" or yy == "48"):
+                modays = leap_days
+            # Month loop
+            for mo in range(0, 12, 1):
+                if (julday > modays[mo] and julday <= modays[mo+1]):
+                    day = (julday - modays[mo])
+                    # Set month and day strings
+                    if (mo < 9):
+                        mon = "0"+str(mo+1)
                     else:
-		        mon = str(mo+1)
-                    if (day<10):
+                        mon = str(mo+1)
+                    if (day < 10):
                         day = "0"+str(day)
                     else:
                         day = str(day)
-		    infiles = sorted(glob.glob(data_dir+"/GLDAS_NOAH025_3H.A"+cc[0]+yrs[yy]+(mon)+(day)+"*"))
-		    if (len(infiles)>0):
-			intime      = cc[0]+yrs[yy]+(mon)+(day) 
-			infile_list = infiles
-    if not os.path.exists(results_dir+"/"+vars_short[var]):
-        os.system("mkdir "+results_dir+"/"+vars_short[var])
-    for hr in range(0,8):
-        infile = infile_list[hr]
-	print "working on file: ",intime
-        outfile= results_dir+"/"+vars_short[var]+"/GLDAS_"+vars_name[var]+"_"+intime+hrs[hr]
-	if not os.path.exists(outfile):
-	    print "working on file: ",infile
-            os.system("ncks -v "+vars_name[var]+" "+infile+" "+outfile)
-        else:
-	    print "file exist, move to next one"
+                    # Get ini files
+                    infiles = sorted(glob.glob(
+                        data_dir + filestem + cc[0] + yrs[yy] + (mon) + (day)
+                        + "*"))
+                    if (len(infiles) > 0):
+                        intime = cc[0]+yrs[yy]+(mon)+(day)
+                        infile_list = infiles
+                    if not os.path.exists(results_dir + vars_short[var]):
+                        os.system("mkdir " + results_dir + vars_short[var])
+                    for hr in range(0, 8):
+                        infile = infile_list[hr]
+                        outfile = results_dir + vars_short[var] + "/GLDAS_" + \
+                            vars_name[var] + "_" + intime + hrs[hr]
+                        if not os.path.exists(outfile):
+                            print("writing file: ", outfile)
+                            os.system("ncks -v " + vars_name[var] + " "
+                                      + infile + " " + outfile)
+                        else:
+                            print("file exist, move to next one")
 
-print "Successfully extract necessary variables!"
+print("Successfully extracted necessary variables!")
